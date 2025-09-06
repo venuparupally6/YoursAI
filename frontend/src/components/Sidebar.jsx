2943b4aaa6558b5f8c283b1749f099649590795a
Venu <venuparupally6@gmail.com>
2025-09-06 08:35:40 +0530

    update in the sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -19,7 +19,7 @@ const Sidebar = ({sidebar, setSidebar}) => {
   return (
     <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between
     items-center max-sm:absolute top-14 bottom-0 ${sidebar ? 'translate-x-0': 'max-sm:-translate-x-full'} transition-all duration-300
-    ease-in-out`}>
+    ease-in-out z-10`}>
         <div className='my-7 w-full'>
             <img src={user.imageUrl} alt="User avatar" className='w-13 rounded-full mx-auto'/>
             <h1 className='mt-2 text-center'>{user.fullName}</h1>
